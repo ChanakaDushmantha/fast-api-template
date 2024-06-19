d720825b93b8d99e99e3ddd99a9f6b2c8a423400
Chanaka Dushmantha <chanakadushmantha@codecommit-at-789734661575>
2024-06-19 16:23:10 +0530

    add rest client

---
+++ app/sp/rest_client.py
@@ -0,0 +1,32 @@
+from logging import getLogger
+from typing import List
+
+import httpx
+
+from app.exceptions.exception import RestClientException, UnauthorizedException
+
+logger = getLogger(__name__)
+
+
+async def get_request(base_url: str, endpoint: str = None, headers: dict = None, params: dict = None):
+    async with httpx.AsyncClient() as client:
+        url = f"{base_url}{endpoint if endpoint else ''}"
+        logger.debug(f"Request URL: {url}{' params: ' + str(params) if params else ''}")
+        response = await client.get(url, headers=headers, params=params)
+        if response.status_code == 401:
+            raise UnauthorizedException(message=response.text, status=response.status_code)
+        if response.status_code != 200:
+            raise RestClientException(message=response.text, status=response.status_code)
+        return response.json()
+
+
+async def post_request(base_url: str, endpoint: str = None, headers: dict = None, data: dict | List = None):
+    async with httpx.AsyncClient() as client:
+        url = f"{base_url}{endpoint if endpoint else ''}"
+        logger.debug(f"Request URL: {url} data: {data}")
+        response = await client.post(url, headers=headers, json=data)
+        if response.status_code == 401:
+            raise UnauthorizedException(message=response.text, status=response.status_code)
+        if response.status_code != 200:
+            raise RestClientException(message=response.text, status=response.status_code)
+        return response.json()
