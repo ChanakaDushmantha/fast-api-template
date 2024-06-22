e65744c1ea351136792c13f7c8e1f7ce2a37ad7a
Chanaka Dushmantha <chanakadushmantha@codecommit-at-789734661575>
2024-06-22 17:28:27 +0530

    update exception handler

--- app/exception/exception.py
+++ app/exception/exception.py
@@ -1,3 +1,15 @@
 class NoDataFoundException(Exception):
     def __init__(self, message: str):
         self.message = message
+
+
+class RestClientException(Exception):
+    def __init__(self, message: str, status: int):
+        self.status = status
+        self.message = message
+
+
+class UnauthorizedException(Exception):
+    def __init__(self, message: str, status: int):
+        self.status = status
+        self.message = message
--- app/exception/exception_handler.py
+++ app/exception/exception_handler.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import IntegrityError
 from starlette.responses import JSONResponse
 from starlette.status import HTTP_400_BAD_REQUEST
 
-from exception.exception import NoDataFoundException
+from exception.exception import NoDataFoundException, RestClientException, UnauthorizedException
 
 logger = getLogger(__name__)
 
@@ -30,3 +30,19 @@ def add_exception_handler(app: FastAPI):
         return JSONResponse(status_code=HTTP_400_BAD_REQUEST,
                             content={'success': False,
                                      'message': str(exc)})
+
+    @app.exception_handler(RestClientException)
+    async def handle_no_data_found_exception(request: Request, exc: RestClientException):
+        logger.error(exc.message)
+        return JSONResponse(status_code=HTTP_400_BAD_REQUEST,
+                            content={'success': False,
+                                     'message': exc.message,
+                                     'status_code': exc.status})
+
+    @app.exception_handler(UnauthorizedException)
+    async def handle_unauthorized_exception(request: Request, exc: UnauthorizedException):
+        logger.error(exc.message)
+        return JSONResponse(status_code=HTTP_400_BAD_REQUEST,
+                            content={'success': False,
+                                     'message': exc.message,
+                                     'status_code': exc.status})
