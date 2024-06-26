fa4d391a627637e69b84fb3cf0b5b1b0565f54b0
Chanaka Dushmantha <dushmantha.sse@gmail.com>
2024-06-26 17:00:44 +0530

    add doc string

--- app/sp/rest_client.py
+++ app/sp/rest_client.py
@@ -1,5 +1,4 @@
 from logging import getLogger
-from typing import List
 
 import httpx
 
@@ -9,6 +8,22 @@ logger = getLogger(__name__)
 
 
 async def get_request(base_url: str, endpoint: str = None, headers: dict = None, params: dict = None):
+    """
+    Make an asynchronous GET request to the specified endpoint.
+
+    Args:
+        base_url (str): The base URL of the API.
+        endpoint (str, optional): The specific endpoint to append to the base URL. Defaults to None.
+        headers (dict, optional): Headers to include in the request. Defaults to None.
+        params (dict, optional): Query parameters to include in the request. Defaults to None.
+
+    Returns:
+        dict: The JSON response from the API.
+
+    Raises:
+        UnauthorizedException: If the response status code is 401.
+        RestClientException: If the response status code is not 200.
+    """
     async with httpx.AsyncClient() as client:
         url = f"{base_url}{endpoint if endpoint else ''}"
         logger.debug(f"Request URL: {url}{' params: ' + str(params) if params else ''}")
@@ -20,7 +35,23 @@ async def get_request(base_url: str, endpoint: str = None, headers: dict = None,
         return response.json()
 
 
-async def post_request(base_url: str, endpoint: str = None, headers: dict = None, data: dict | List = None):
+async def post_request(base_url: str, endpoint: str = None, headers: dict = None, data: dict | list = None):
+    """
+    Make an asynchronous POST request to the specified endpoint.
+
+    Args:
+        base_url (str): The base URL of the API.
+        endpoint (str, optional): The specific endpoint to append to the base URL. Defaults to None.
+        headers (dict, optional): Headers to include in the request. Defaults to None.
+        data (dict | list, optional): The JSON payload to include in the request body. Defaults to None.
+
+    Returns:
+        dict: The JSON response from the API.
+
+    Raises:
+        UnauthorizedException: If the response status code is 401.
+        RestClientException: If the response status code is not 200.
+    """
     async with httpx.AsyncClient() as client:
         url = f"{base_url}{endpoint if endpoint else ''}"
         logger.debug(f"Request URL: {url} data: {data}")
@@ -30,3 +61,4 @@ async def post_request(base_url: str, endpoint: str = None, headers: dict = None
         if response.status_code != 200:
             raise RestClientException(message=response.text, status=response.status_code)
         return response.json()
+
